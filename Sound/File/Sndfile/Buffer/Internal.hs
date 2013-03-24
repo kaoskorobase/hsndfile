dffdfdc99ce1b6d2359c8164dc58e8ebf11bb8fe
Roman Cheplyaka <roma@ro-che.info>
2013-03-25 00:21:02 +0200

    Samples are signed

--- Sound/File/Sndfile/Buffer/Internal.hs
+++ Sound/File/Sndfile/Buffer/Internal.hs
@@ -3,17 +3,17 @@ module Sound.File.Sndfile.Buffer.Internal
 (
     IOFunc
   , hBufIO
-  , sf_readf_word16
-  , sf_writef_word16
-  , sf_readf_word32
-  , sf_writef_word32
+  , sf_readf_int16
+  , sf_writef_int16
+  , sf_readf_int32
+  , sf_writef_int32
   , sf_readf_float
   , sf_writef_float
   , sf_readf_double
   , sf_writef_double
 ) where
 
-import Data.Word					(Word16, Word32)
+import Data.Int (Int16, Int32)
 import Foreign.Ptr     				(Ptr)
 import Foreign.C.Types 				(CLLong(..))
 import Sound.File.Sndfile.Interface (Count, Handle(..), HandlePtr)
@@ -23,11 +23,11 @@ type IOFunc a = HandlePtr -> Ptr a -> CLLong -> IO CLLong
 hBufIO :: IOFunc a -> Handle -> Ptr a -> Count -> IO Count
 hBufIO f h ptr = fmap fromIntegral . f (hPtr h) ptr . fromIntegral
 
-foreign import ccall unsafe "sf_readf_short"   sf_readf_word16  :: IOFunc Word16
-foreign import ccall unsafe "sf_writef_short"  sf_writef_word16 :: IOFunc Word16
+foreign import ccall unsafe "sf_readf_short"   sf_readf_int16  :: IOFunc Int16
+foreign import ccall unsafe "sf_writef_short"  sf_writef_int16 :: IOFunc Int16
 
-foreign import ccall unsafe "sf_readf_int"     sf_readf_word32  :: IOFunc Word32
-foreign import ccall unsafe "sf_writef_int"    sf_writef_word32 :: IOFunc Word32
+foreign import ccall unsafe "sf_readf_int"     sf_readf_int32  :: IOFunc Int32
+foreign import ccall unsafe "sf_writef_int"    sf_writef_int32 :: IOFunc Int32
 
 foreign import ccall unsafe "sf_readf_float"   sf_readf_float   :: IOFunc Float
 foreign import ccall unsafe "sf_writef_float"  sf_writef_float  :: IOFunc Float
